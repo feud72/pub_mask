cc0f7be24bc26fbbb9d23462f4ef79d8d2cb2028
feud72 <feud72@gmail.com>
2020-03-12 13:23:35 +0900

    [Add] postgresql DB

--- config/base.py
+++ config/base.py
@@ -72,10 +72,14 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    #     'default': {
-    #         'ENGINE': 'django.db.backends.sqlite3',
-    #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    #     }
+    "default": {
+        "ENGINE": os.environ.get("DJANGO_WEBSERVER_DB_ENGINE"),
+        "NAME": os.environ.get("DJANGO_WEBSERVER_DB_NAME"),
+        "USER": os.environ.get("DJANGO_WEBSERVER_DB_USER"),
+        "PASSWORD": os.environ.get("DJANGO_WEBSERVER_DB_PASSWORD"),
+        "HOST": os.environ.get("DJANGO_WEBSERVER_DB_HOST"),
+        "PORT": os.environ.get("DJANGO_WEBSERVER_DB_PORT"),
+    }
 }
 
 
